12ad50c580a29cbc22aac7d016d3e61751b26738
Katarzyna Kaldunska <k.kaduska@gmail.com>
2018-10-03 22:09:00 +0200

    add backend

---
+++ backend/db.js
@@ -0,0 +1,26 @@
+var mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+mongoose.connect(
+  'mongodb://localhost:27017/TodoApp',
+  {useNewUrlParser: true}
+);
+
+var Todo = mongoose.model('Todo', {
+  content: {
+    type: String,
+    required: true,
+    minLength: 1,
+    trim: true
+  },
+  finished: {
+    type: Boolean,
+    default: false
+  },
+  sort: {
+    type: Number,
+    default: 0
+  }
+});
+
+module.exports = {mongoose, Todo};
---
+++ backend/server.js
@@ -0,0 +1,86 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
+
+var {mongoose, Todo} = require('./db');
+
+var app = express();
+const port = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+
+// POST /todos
+app.post('/todos', (req, res) => {
+  var todo = new Todo({
+    content: req.body.content
+  });
+
+  todo.save().then((todo) => {
+    res.send(todo._id);
+  }, (e) => {
+    res.status(400).send(e);
+  });
+});
+
+// GET /todos
+app.get('/todos', (req, res) => {
+  Todo.find().then((todos) => {
+    res.send(todos);
+  }, (e) => {
+    res.status(400).send(e);
+  })
+});
+
+// GET /todos/:id
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Todo.findById(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+
+    res.send(todo);
+  }).catch(err => res.status(400).send());
+});
+
+// DELETE /todos/:id
+app.delete('/todos/:id', (req, res) => {
+  var id = req.params.id;
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Todo.findOneAndDelete(id).then((todo) => {
+    if (todo) {
+      return res.status(200).send(todo);
+    }
+    return res.status(404).send();
+
+  }).catch(err => res.status(400).send());
+});
+
+// PATCH /todos/:id
+app.patch('/todos/:id', (req, res) => {
+  var id = req.params.id;
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+  Todo.findOneAndUpdate(id, {$set: req.body}, {new: true}).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+    return res.send(todo);
+  }). catch(e => res.status(400).send());
+})
+
+
+app.listen(port, () => {
+  console.log(`Started on port ${port}`);
+});
+
+module.exports = {app};
